28931b59a06ace2b6155a3eb16264bcafebcf03a
John Garner <zellasphere@gmail.com>
2024-04-23 19:40:03 -0400

    updated routes and added navbar

--- app.py
+++ app.py
@@ -3,8 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('index.html')
+
+@app.route('/contact')
+def contact():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
