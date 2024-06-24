27542a7b116f38841e1a04ea036c6749fd009604
John Garner <zellasphere@gmail.com>
2024-06-24 00:07:11 -0400

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 export default defineConfig({
-  site: 'https://tenseicho.github.io',
+  site: 'https://tenseicho.xyz/',
   base: '/',
   outDir: './dist',
 });
